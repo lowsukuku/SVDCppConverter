247ec67c50b2322d666d25ffc8f2b9cf26c47981
lowsukuku <gordon970530@gmail.com>
2019-07-29 23:38:07 +0300

    initialized base classes and tried xml deserializer

---
+++ Core/Device.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Core
+{
+    [XmlRoot("device")]
+    public class Device
+    {
+        public string name;
+        [XmlArrayItem("peripheral")]
+        public List<Peripheral> peripherals;
+    }
+
+    public class Peripheral
+    {
+        public string name;
+    }
+}
--- SVDCppConverter/Program.cs
+++ SVDCppConverter/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using Core;
 
 namespace SVDCppConverter
 {
@@ -10,6 +13,14 @@ namespace SVDCppConverter
     {
         static void Main(string[] args)
         {
+            string path = @"D:\Dev\Keil\ARM\PACK\Keil\STM32L1xx_DFP\1.2.0\SVD\STM32L1xx.svd";
+            XmlSerializer serializer = new XmlSerializer(typeof(Device));
+            Device device;
+            using (FileStream fs = File.OpenRead(path))
+            {
+                device = (Device)serializer.Deserialize(fs);
+
+            }
         }
     }
 }
